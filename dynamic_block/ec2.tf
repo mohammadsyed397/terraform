45f2121fb1d63e3396644d8a8b9a65f7fd55f778
mohammadsyed397 <mohammad.syed601@gmail.com>
2025-07-02 11:09:07 -0400

    first commit

---
+++ dynamic_block/ec2.tf
@@ -0,0 +1,37 @@
+resource "aws_instance" "roboshop" {
+  # for_each = var.instances
+  for_each =  toset(var.instances)
+  ami                    = var.ami_id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+
+  tags = {
+    Name = each.key
+  }
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = var.sg_name
+  description = var.sg_description
+
+  dynamic ingress {
+    for_each = var.ingress_ports
+    content {
+      from_port = ingress.value["from_port"]
+      to_port = ingress.value["to_port"]
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = var.sg_tags
+}
